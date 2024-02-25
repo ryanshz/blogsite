9b52e3081c39cbffd38fa5132e501e278bd0e1cb
ryanshz <92750669+ryanshz@users.noreply.github.com>
2024-02-25 06:15:30 -0500

    moved astro blogsite from frameworks to this repo

---
+++ tailwind.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+	},
+	daisyui: {
+		themes: ['lofi', 'business', 'synthwave', 'cyberpunk', 'cmyk'],
+	},
+	plugins: [require('@tailwindcss/typography'), require('daisyui')],
+}
